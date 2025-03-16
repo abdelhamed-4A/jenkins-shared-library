5b1e059dc23c355acb6231a0910301389f18f9e0
Abdel-Hamed Abdel-Nasser <abdelhamednasser2@gmail.com>
2025-03-16 02:10:24 +0200

    Upload Shared Library

--- vars/buildAndDeploy.groovy
+++ vars/buildAndDeploy.groovy
@@ -1,25 +1,27 @@
 // vars/buildAndDeploy.groovy
 def call(Map config) {
     // Clone the repository
-    git url: config.repositoryUrl, branch: config.branch, credentialsId: config.gitCredentialsId
+    git url: 'https://github.com/abdelhamed-4A/NTI-IVolve-Training.git', branch: 'main', credentialsId: 'github-token'
 
     // Build Docker image
-    sh "docker build -t ${config.dockerImageName}:${config.dockerImageTag} ."
+    sh "docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} Jenkins/Task-3/."
 
     // Push Docker image to Docker Hub
-    withCredentials([usernamePassword(credentialsId: config.dockerHubCredentialsId, usernameVariable: 'DOCKER_HUB_USER', passwordVariable: 'DOCKER_HUB_PASSWORD')]) {
+    withCredentials([usernamePassword(credentialsId: 'DockerHub', usernameVariable: 'DOCKER_HUB_USER', passwordVariable: 'DOCKER_HUB_PASSWORD')]) {
         sh "echo ${DOCKER_HUB_PASSWORD} | docker login -u ${DOCKER_HUB_USER} --password-stdin"
-        sh "docker push ${config.dockerImageName}:${config.dockerImageTag}"
+        sh "docker push ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
     }
 
     // Delete local Docker image
-    sh "docker rmi ${config.dockerImageName}:${config.dockerImageTag}"
+    sh "docker rmi ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
 
     // Update Kubernetes deployment
     sh """
-        sed -i 's|image: .*|image: ${config.dockerImageName}:${config.dockerImageTag}|g' ${config.kubeDeploymentFile}
+        kubectl config set-credentials jenkins-user --token=${K8S_TOKEN}
+                kubectl config set-cluster minikube --server=https://192.168.49.2:8443 --insecure-skip-tls-verify
+                kubectl config set-context jenkins-context --cluster=my-cluster --user=jenkins-user
+                kubectl config use-context jenkins-context
+                kubectl apply -f Jenkins/Task-3/deployment.yaml
     """
 
-    // Deploy to Kubernetes
-    sh "kubectl apply -f ${config.kubeDeploymentFile}"
 }
